f86c36111f8a17d87679cb4a1fae5487677a52f7
CollinPatrick03 <collinpatrick2003@gmail.com>
2023-03-12 22:45:33 -0400

    Finished restructure, time to begin command and structure implementation

--- src/main/java/me/collinpatrick/customcraft/CustomCraft.java
+++ src/main/java/me/collinpatrick/customcraft/CustomCraft.java
@@ -1,18 +1,8 @@
 package me.collinpatrick.customcraft;
 import me.collinpatrick.customcraft.Listeners.PlayerJoinListener;
 import me.collinpatrick.customcraft.RecipesContainer.RecipeContainer;
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.inventory.FurnaceRecipe;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.RecipeChoice;
-import org.bukkit.inventory.ShapedRecipe;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.*;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
@@ -27,16 +17,6 @@ public final class CustomCraft extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
-    }
-
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if(command.getName().equalsIgnoreCase("moo")) {
-            for(Player p : getServer().getOnlinePlayers()) {
-                p.sendMessage("MOOOOOOOOOO");
-            }
-        }
-        return true;
     }
 
 }
---
+++ src/main/java/me/collinpatrick/customcraft/Models/StoredBuilding.java
@@ -0,0 +1,5 @@
+package me.collinpatrick.customcraft.Models;
+
+public class StoredBuilding {
+
+}
